806d5c5ff075b38a2c1e590d813e23528b8c1a9b
Akhil Chauhan <akhil@mezeh.net>
2023-10-22 21:10:48 -0400

    added title

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,11 +7,11 @@ const settings = {
 
 //SEO Related settings
 const seo = {
-  title: "Ashutosh's Portfolio",
+  title: "Akhil's Portfolio",
   description:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   og: {
-    title: "Ashutosh Hathidara Portfolio",
+    title: "Akhil Singh Chauhan",
     type: "website",
     url: "http://ashutoshhathidara.com/",
   },
@@ -19,9 +19,9 @@ const seo = {
 
 //Home Page
 const greeting = {
-  title: "Ashutosh Hathidara",
-  logo_name: "AshutoshHathidara",
-  nickname: "layman_brother",
+  title: "Akhil Singh Chauhan",
+  logo_name: "AkhilSinghChauhan",
+  nickname: "techie",
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
